c37ad3ed914e994f9d1e7478f3a9c005a1d5f4cd
IllTamer <mail@illtamer.com>
2024-03-30 22:27:22 +0800

    ✨ feat: add schedule task

--- RssApp.ts
+++ RssApp.ts
@@ -5,7 +5,9 @@ import {
 } from '@rocket.chat/apps-engine/definition/accessors';
 import { App } from '@rocket.chat/apps-engine/definition/App';
 import { IAppInfo } from '@rocket.chat/apps-engine/definition/metadata';
+import { StartupType } from '@rocket.chat/apps-engine/definition/scheduler';
 import { RssCommand } from './commands/RssCommand';
+import { onUpdate } from './scheduler/RssUpdater';
 
 export class RssApp extends App {
 
@@ -16,5 +18,16 @@ export class RssApp extends App {
     public async extendConfiguration(configuration: IConfigurationExtend) {
         await configuration.slashCommands.provideSlashCommand(
             new RssCommand(this));
+
+        configuration.scheduler.registerProcessors([
+            {
+                id: 'update',
+                processor: onUpdate,
+                startupSetting: {
+                    type: StartupType.RECURRING,
+                    interval: '60 seconds',
+                }
+            },
+        ]);
     }
 }
